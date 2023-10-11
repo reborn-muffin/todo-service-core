ca8af7d2685c09dea78fdee1d5cae48c2d0294b3
Bohdan <reborn.muffin@gmai.com>
2023-10-12 01:01:50 +0300

    initial commit

---
+++ src/main/java/com/todo/todocoreservice/TodoCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todocoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoCoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todo/todocoreservice/TodoCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todocoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
